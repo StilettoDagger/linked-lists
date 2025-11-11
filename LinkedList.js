395ff043c258fd316f81772a6f61e5b897a0983b
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2025-11-12 00:42:23 +0300

    refactor: move tests to main.js

--- LinkedList.js
+++ LinkedList.js
@@ -1,6 +1,6 @@
 import Node from "./Node.js";
 
-class LinkedList {
+export default class LinkedList {
 	constructor() {
 		this.size = 0;
 		this.head = null;
@@ -145,61 +145,3 @@ class LinkedList {
 		return curr;
 	}
 }
-
-// Tests
-
-// const list = new LinkedList();
-
-// list.append(4);
-// list.append(5);
-// list.prepend(1);
-
-
-
-// console.log(list.toString());
-// console.log(list.size);
-
-// console.log(list.at(0));
-// console.log(list.at(1));
-// console.log(list.at(2));
-
-// console.log(list.pop());
-// console.log(list.toString());
-// console.log(list.size);
-
-// console.log(list.toString());
-
-// console.log(list.contains(5));
-// console.log(list.contains(4));
-
-// console.log(list.find(4));
-// console.log(list.toString());
-// list.insertAt(3, 1);
-// console.log(list.toString());
-// console.log(list.size);
-// console.log(list.removeAt(2));
-// console.log(list.toString());
-// console.log(list.size);
-// list.prepend(5);
-// console.log(list.toString());
-// console.log(list.size);
-
-// list.append(8);
-// console.log(list.toString());
-
-
-// console.log(list.find(5));
-// console.log(list.find(3));
-
-// console.log(list.size);
-
-
-// list.pop();
-// list.pop();
-// list.pop();
-// console.log(list.toString());
-// console.log(list.size);
-// list.pop();
-// list.pop();
-
-// console.log(list.toString());
